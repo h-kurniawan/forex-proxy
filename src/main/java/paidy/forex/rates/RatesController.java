206f9a0ce24c14e3b949f319ea91593d39592e00
Herry Kurniawan <hkurniawan@mdsol.com>
2022-09-06 00:50:43 +0900

    Add support to accept 'pair' query params

--- src/main/java/paidy/forex/rates/RatesController.java
+++ src/main/java/paidy/forex/rates/RatesController.java
@@ -5,9 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,8 +17,8 @@ public class RatesController {
     private final ForeignExchange forex;
 
     @GetMapping
-    ResponseEntity<List<ExchangeRate>> getRates() throws JsonProcessingException {
-        var rates = forex.getRates();
-        return ResponseEntity.ok(rates);
+    ResponseEntity<List<ExchangeRate>> getRates(@RequestParam("pair") List<String> currencyPairs) {
+        var response = forex.getRates(currencyPairs);
+        return ResponseEntity.ok(response.getBody());
     }
 }
