e8c394088e0e8c2d63a0c4f07938ce2935d45370
Herry Kurniawan <hkurniawan@mdsol.com>
2022-09-06 22:39:06 +0900

    Fix RatesControllerTest

--- src/main/java/paidy/forex/rates/RatesController.java
+++ src/main/java/paidy/forex/rates/RatesController.java
@@ -1,14 +1,13 @@
 package paidy.forex.rates;
 
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
--- src/test/java/paidy/forex/rates/RatesControllerTest.java
+++ src/test/java/paidy/forex/rates/RatesControllerTest.java
@@ -1,12 +1,5 @@
 package paidy.forex.rates;
 
-import static org.assertj.core.api.BDDAssertions.then;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import java.time.Instant;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureJsonTesters
@@ -37,12 +38,13 @@ public class RatesControllerTest {
         List<ExchangeRate> rates = List.of(
             new ExchangeRate("USD", "JPY", 0.61F, 0.82F, 0.71F, Instant.parse("2019-01-01T00:00:00.000Z"))
         );
-        given(forex.getRates())
-            .willReturn(rates);
+        var expectedResponse = new ResponseEntity<>(rates, HttpStatus.OK);
+        given(forex.getRates(List.of("USDJPY")))
+            .willReturn(expectedResponse);
 
         // when
         var response = mvc
-            .perform(get("/rates"))
+            .perform(get("/rates?pair=USDJPY"))
             .andReturn()
             .getResponse();
         
